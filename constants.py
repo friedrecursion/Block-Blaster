8eab999dc6dd719c38257e90054700d6f673d2fd
elias <elimeynckens@gmail.com>
2024-10-30 00:52:54 +0100

    added correct color scheme

--- constants.py
+++ constants.py
@@ -1,10 +1,11 @@
 # board constants
 N = 8       
 CELL_SIZE = 100
-BACKGROUND_COLOR = (18, 22, 70) # Dark Blue
+BACKGROUND_COLOR = (60,74,128) # Dark Blue
+EMPTY_TILE_COLOR = (32,36,69)
 # Derived constants
 LINE_THICKNESS = CELL_SIZE//20
-LINE_COLOR = tuple(BACKGROUND_COLOR[i]//2 for i in range(3)) # Darker Blue
+LINE_COLOR = (25,32,59)
 WIDTH, HEIGHT = N * CELL_SIZE, N * CELL_SIZE
 
 HOVER_ALPHA = 100 # Brightness of Block hover
@@ -13,25 +14,27 @@ OUT_OF_BOUND = -1 # Out of Grid value
 
 # Possible colors to display
 EMPTY = 0
-BLUE = 1
-RED = 2
-GREEN = 3
-PURPLE = 4
+PURPLE = 1
+BLUE = 2
+LIGHT_BLUE = 3
+GREEN = 4
 YELLOW = 5
 ORANGE = 6
+RED = 7
 
 # Possible Block colors
 COLORS = [BLUE,RED,GREEN,PURPLE,ORANGE]
 
 # Color MACRO to RGB conversion
 RGB_COLORS = {
-    EMPTY : BACKGROUND_COLOR,
-    BLUE : (53, 178, 224),     # Muted Blue
-    RED : (202, 50, 49),      # Muted Red
-    GREEN : (58, 180, 59),    # Muted Green
-    PURPLE : (210, 95, 214),   # Muted Purple
-    YELLOW : (236, 182, 50),   # Muted Yellow
-    ORANGE : (220, 180, 100),   # Muted Orange
+    EMPTY : EMPTY_TILE_COLOR,
+    PURPLE : (141,92,211),
+    BLUE : (75,99,230),
+    LIGHT_BLUE : (63,178,226),
+    GREEN : (80,194,91),
+    YELLOW : (235,185,58),
+    ORANGE : (238,125,40),
+    RED : (217,62,60),
 }
 
 # SHAPES
